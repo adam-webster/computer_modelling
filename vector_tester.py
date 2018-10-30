5dad36d2261f12f68c14f5503858f56b1cf88d2f
adam-webster <43613194+adam-webster@users.noreply.github.com>
2018-10-30 12:04:04 +0000

    Update vector_tester.py

--- vector_tester.py
+++ vector_tester.py
@@ -1,33 +1,32 @@
 import math
-
 import random
-
 import vector
 
 '''
     this program randomly creates vectors, tells you what the vectors are
-    then shows how simple vector calculations work. it also tests the following
-    identities and show if they hold true or not:
-    v1 x v2 = - v2 x v1
-    v1 ×(v2 + v3) = (v1 x v2) + (v1 × v3)
-    v1 ×(v2 × v3) = (v1 · v3)v2 − (v1 · v2)v3
 
+    then shows how simple vector calculations work. it also tests the following
+
+    identities and show if they hold true or not:
+
+    v1 x v2 = - v2 x v1
+
+    v1 ×(v2 + v3) = (v1 x v2) + (v1 × v3)
+
+    v1 ×(v2 × v3) = (v1 · v3)v2 − (v1 · v2)v3
 '''
 
 
-
 def identity_1(v1, v2):
-
     #v1 x v2 = - v2 x v1
+    v3 = vector.cross(v1, v2) #lhs
+    v4 = vector.cross(v2, v1)
+    v4 = vector.scalar_mult(v4, -1) #rhs
 
-    if vector.cross(v1, v2)[0]+vector.cross(v2, v1)[0]==0 and vector.cross(v1, v2)[1]+vector.cross(v2, v1)[1]==0 and vector.cross(v1, v2)[2]+vector.cross(v2, v1)[2]==0:
-
+    if v3[0]==v4[0] and v3[1]==v4[1] and v3[2]==v4[2]:
         print('v1 x v2 = - v2 x v1')
-
     else:
-
         print('v1 x v2 does not equal - v2 x v1')
-
 
 def identity_2(v1, v2, v3):
 
@@ -35,9 +34,11 @@ def identity_2(v1, v2, v3):
 
     v4 = vector.sum(v2, v3)
     v4 = vector.cross(v1, v4) #lhs
+    v4 = [round(elm, 10) for elm in v4] #elements need to be rounded to be exactly equal
     v5 = vector.cross(v1,v2)
     v6 = vector.cross(v1, v3)
-    v7 = vector.sum(v5, v6) #rhs
+    v7 = vector.sum(v5, v6)
+    v7 = [round(elm, 10) for elm in v7] #rhs
 
     if v4[0]==v7[0] and v4[1]==v7[1] and v4[2]==v7[2]:
         print('v1 ×(v2 + v3) = (v1 x v2) + (v1 × v3)')
@@ -51,18 +52,19 @@ def identity_3(v1, v2, v3):
 
     v4 = vector.cross(v2, v3)
     v4 = vector.cross(v1, v4) #lhs
+    v4 = [round(elm, 10) for elm in v4] #lhs rounded
     s5 = vector.dot(v1, v3)
     s6 = vector.dot(v1, v2)
     s6 = -s6
     v5 = vector.scalar_mult(v2, s5)
     v6 = vector.scalar_mult(v3, s6)
     v7 = vector.sum(v5, v6) #rhs
+    v7 = [round(elm, 10) for elm in v7] #rhs rounded
 
     if v4[0]==v7[0] and v4[1]==v7[1] and v4[2]==v7[2]:
         print('v1 ×(v2 × v3) = (v1 · v3)v2 − (v1 · v2)v3')
     else:
         print('v1 ×(v2 × v3) does not equal (v1 · v3)v2 − (v1 · v2)v3')
-
 
 
 # Main method:
@@ -72,40 +74,35 @@ def main():
     # Create two random vectors
 
     v1 = [random.random(), random.random(), random.random()]
-
     v2 = [random.random(), random.random(), random.random()]
-
     v3 = [random.random(), random.random(), random.random()]
 
-
     #line break in output screen, clearer to read
+
     print()
 
 
     # Print out the vectors to terminal window
 
     print("v1 = ", v1)
-
     print("v2 = ", v2)
-
     print("v3 = ", v3)
 
 
     #line break in output screen, clearer to read
-    print()
 
+    print()
 
     #print basic vector manipulations
 
     #vector sum, scalar product and vector (cross) product
 
     print("v1 + v2 =", vector.sum(v1, v2))
-
     print('v1 · v2 =', vector.dot(v1, v2))
-
     print('v1 x v2 =', vector.cross(v1, v2))
 
     #line break in output screen, clearer to read
+
     print()
 
 
@@ -114,7 +111,6 @@ def main():
     identity_1(v1, v2)
     identity_2(v1, v2, v3)
     identity_3(v1, v2, v3)
-
 
 
 # Execute main method
